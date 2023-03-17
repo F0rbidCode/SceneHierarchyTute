37280584137f292b97fdd0cd7a1ce39d97fce4ec
mike93 <mike.2593@hotmail.com>
2023-03-17 16:30:57 +1300

    added bullet sprtie and scene objects and got them to spawn and move from the turret,    need to get rotation sorted

--- SceneHierarchyTute/Game.cs
+++ SceneHierarchyTute/Game.cs
@@ -21,12 +21,20 @@ namespace SceneHierarchyTute
 
         private float deltaTime = 0.005f;
 
+        //create a variable to know if shot has occurd
+        bool shot = false;
+
         //create new scene and sprite objects for tank body and tank turret
         SceneObject tankObject = new SceneObject();
         SceneObject turretObject = new SceneObject();
 
         SpriteObject tankSprite = new SpriteObject();
         SpriteObject turretSprite = new SpriteObject();
+
+        //create new Scene and Sprite objects for bullet
+        SceneObject bulletObject = new SceneObject();
+        SpriteObject bulletSprite = new SpriteObject();
+
 
         public void Init()
         {
@@ -39,7 +47,7 @@ namespace SceneHierarchyTute
             tankSprite.SetRotate(-90 * (float)(Math.PI / 180.0f));
             //set an offset for the base of the tank so that it rotates around the centre
             tankSprite.SetPosition(-tankSprite.Width / 2.0f, tankSprite.Height / 2.0f);
-            //tankSprite.SetPosition(50, 50);
+            
 
             //load the image for the tank turret
             turretSprite.Load("barrelBlue.png");
@@ -48,10 +56,15 @@ namespace SceneHierarchyTute
             // set the turret offset from the tank base
             turretSprite.SetPosition(0, turretSprite.Width / 2.0f);
 
+            
+
             //set the scene object hierarchy
             turretObject.AddChild(turretSprite); //set the turret sprite to be a child of the turret object
             tankObject.AddChild(tankSprite); //set the tank sprite to be a child of the tank object
             tankObject.AddChild(turretObject); //set the turret object to be a child of the tank object
+
+            bulletObject.AddChild(bulletSprite);
+            //turretObject.AddChild(bulletObject);
 
             //set the position of the tank to the centre of the sceen
             tankObject.SetPosition(GetScreenWidth()/2.0f, GetScreenHeight()/2.0f);          
@@ -112,8 +125,27 @@ namespace SceneHierarchyTute
                 turretObject.Rotate(deltaTime);
             }
 
+            //get user inpuit to fire bullet
+            if (IsKeyDown (KeyboardKey.KEY_SPACE))
+            {
+                if (!shot)
+                {
+                    Shoot();
+                    shot = true;
+                }
+               
+            }
+
             //call update on tank object
             tankObject.Update(deltaTime);
+            if (shot)
+            {
+                Vector3 facing = new Vector3(
+                    bulletObject.LocalTransform.m00,
+                    bulletObject.LocalTransform.m01, 1) * deltaTime * 100;
+                bulletObject.Translate(facing.x, facing.y);
+                bulletObject.Update(deltaTime);
+            }
 
             lastTime = currentTime;
         }
@@ -127,8 +159,41 @@ namespace SceneHierarchyTute
 
             //call to draw the tank object
             tankObject.Draw();
-
+            bulletObject.Draw();
             EndDrawing();
+        }
+
+        //create a function to spawn and shoot a bullet when the space bar is pressed
+        public void Shoot()
+        {
+            //load the image for the bullet sprite
+            bulletSprite.Load("bulletBlueSilver_outline.png");
+           //set positions and rotation for bullet sprite
+            bulletSprite.SetRotate(90 * (float)(Math.PI / 180.0f));
+            bulletObject.SetPosition(turretObject.GlobalTransform.m20 + turretSprite.Height, turretObject.GlobalTransform.m21 + (-turretSprite.Width / 2.0f));
+           
+
+            
+
+
+            turretObject.RemoveChild(bulletObject);
+
+
+
+
+            //turretObject.AddChild(bulletSprite);a
+
+
+            //bulletSprite.SetPosition(turretSprite.Width / 2.0f, turretSprite.Height);
+
+            //turretObject.RemoveChild(bulletSprite);
+
+            //attach sprite object to scene object hirachy
+            //bulletObject.AddChild(bulletSprite);
+            //turretSprite.AddChild(bulletSprite);
+
+
+
         }
     }
 }
